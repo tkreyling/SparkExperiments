a98e76e3b9d6df3a650e2d3a2b1b63ade6b73f36
Thomas Kreyling <Thomas.Kreyling@googlemail.com>
2015-11-06 10:39:35 +0100

    added method leftOuterJoinList to enrich data stream by additional stream

--- src/main/java/kreyling/sparkexperiments/Exp1.java
+++ src/main/java/kreyling/sparkexperiments/Exp1.java
@@ -7,9 +7,13 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
 import scala.Tuple2;
 
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.function.BiFunction;
 
 public class Exp1 {
 
@@ -17,16 +21,28 @@ public class Exp1 {
         public final int id;
         public final String forename;
         public final String surname;
+        public final Iterable<String> knowledge;
+        public final Iterable<String> interests;
 
-        public Person(int id, String forename, String surname) {
+        public Person(int id, String forename, String surname, Iterable<String> knowledge, Iterable<String> interests) {
             this.id = id;
             this.forename = forename;
             this.surname = surname;
+            this.knowledge = knowledge;
+            this.interests = interests;
+        }
+
+        public static Person enrichWithKnowledge(Person person, Iterable<String> knowledge) {
+            return new Person(person.id, person.forename, person.surname, knowledge, person.interests);
+        }
+
+        public static Person enrichWithInterests(Person person, Iterable<String> interests) {
+            return new Person(person.id, person.forename, person.surname, person.knowledge, interests);
         }
 
         public static Person fromCsv(String csvLine) {
             String[] attributes = csvLine.split(",");
-            return new Person(Integer.parseInt(attributes[0]), attributes[1], attributes[2]);
+            return new Person(Integer.parseInt(attributes[0]), attributes[1], attributes[2], Collections.emptyList(), Collections.emptyList());
         }
 
         @Override
@@ -41,20 +57,30 @@ public class Exp1 {
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<Person> persons = sc.textFile("src/main/resources/persons.csv").map(Person::fromCsv);
-        JavaRDD<String> knowledge = sc.textFile("src/main/resources/knowledge.csv");
+        JavaRDD<String[]> knowledge = sc.textFile("src/main/resources/knowledge.csv").map(s -> s.split(","));
+        JavaRDD<String[]> interests = sc.textFile("src/main/resources/interests.csv").map(s -> s.split(","));
 
-        JavaPairRDD<Integer, Person> personsWithId = persons.mapToPair(p -> new Tuple2(p.id, p));
-        JavaPairRDD<Integer, String> knowledgeWithId = knowledge.map(s -> s.split(",")).mapToPair(line -> new Tuple2(Integer.parseInt(line[0]), line[1]));
+        JavaPairRDD<Integer, Person> personsWithId = persons.mapToPair(p -> new Tuple2<>(p.id, p));
+        JavaPairRDD<Integer, String> knowledgeWithId = knowledge.mapToPair(line -> new Tuple2<>(Integer.parseInt(line[0]), line[1]));
+        JavaPairRDD<Integer, String> interestsWithId = interests.mapToPair(line -> new Tuple2<>(Integer.parseInt(line[0]), line[1]));
 
-        persons.collect().forEach(System.out::println);
-        knowledge.collect().forEach(System.out::println);
+        personsWithId = leftOuterJoinList(personsWithId, knowledgeWithId, Person::enrichWithKnowledge);
+        personsWithId = leftOuterJoinList(personsWithId, interestsWithId, Person::enrichWithInterests);
 
-        JavaPairRDD<Integer, Tuple2<Person, Optional<Iterable<String>>>> personsWithListOfKnowledge =
-                personsWithId.leftOuterJoin(knowledgeWithId.groupByKey());
+        System.out.println(personsWithId.toDebugString());
 
-        personsWithListOfKnowledge.collect().forEach(System.out::println);
+        personsWithId.collect().forEach(System.out::println);
         long end = System.currentTimeMillis();
 
         System.out.println(end - start);
+    }
+
+    private static <K, L, R> JavaPairRDD<K, L> leftOuterJoinList(
+            JavaPairRDD<K, L> leftItems,
+            JavaPairRDD<K, R> rightItems,
+            Function2<L, Iterable<R>, L> copyConstructor) {
+        return leftItems
+                .leftOuterJoin(rightItems.groupByKey())
+                .mapValues(tuple -> copyConstructor.call(tuple._1(), tuple._2().or(Collections.emptyList())));
     }
 }
